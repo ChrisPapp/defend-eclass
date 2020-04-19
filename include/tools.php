b1fd36960dafd3046242a60e5823b6971068d14e
Christos Pappas <chrispappas99@yahoo.gr>
2020-04-19 22:47:50 +0300

    Create powerful debugging

--- include/tools.php
+++ include/tools.php
@@ -597,3 +597,9 @@ function lessonToolsMenu(){
 
 	return $sideMenuGroup;
 }
+
+function printSession() {
+	echo "<h3> PHP List All Session Variables</h3>";
+    foreach ($_SESSION as $key=>$val)
+    echo $key." ".$val."<br/>";
+}
