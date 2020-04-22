af417db8cef9dcba0953dda33b062e121efd1e61
Christos Pappas <chrispappas99@yahoo.gr>
2020-04-22 23:22:48 +0300

    Prevent sql injenction from student assignment comment and filename
    User's name and surname are in the filename, so ' is now also nuked in replace_dangerous_char

--- include/lib/fileUploadLib.inc.php
+++ include/lib/fileUploadLib.inc.php
@@ -51,12 +51,12 @@
 
 /*
  * replaces some dangerous character in a string for HTML use
- * currently: ?*<>\/"|:
+ * currently: ?*<>\/"|:'
  */
 
 function replace_dangerous_char($string)
 {
-	return preg_replace('/[?*<>\\/\\\\"|:]/', '-', $string);
+	return preg_replace('/[?*<>\\/\\\\\'"|:]/', '-', $string);
 }
 
 //------------------------------------------------------------------------------
--- modules/work/work.php
+++ modules/work/work.php
@@ -311,6 +311,7 @@ function submit_work($id) {
 	$secret = work_secret($id);
         $ext = get_file_extension($_FILES['userfile']['name']);
 	$filename = "$secret/$local_name" . (empty($ext)? '': '.' . $ext);
+	$stud_comments = escapeSimple($stud_comments);
 	if (move_uploaded_file($_FILES['userfile']['tmp_name'], "$workPath/$filename")) {
 		$msg2 = "$langUploadSuccess";//to message
 		$group_id = user_group($uid, FALSE);
