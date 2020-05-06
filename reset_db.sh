0e1fd7a12deca85fc17198d34d101617ca255d0e
Christos Pappas <chrispappas99@yahoo.gr>
2020-05-06 14:50:41 +0300

    Remove db_backup from server after resetting db

--- reset_db.sh
+++ reset_db.sh
@@ -2,7 +2,8 @@
 
 if [ $1 ]
 then
-	ssh pappabakaliatis@pappabakaliatis.csec.gr "/usr/local/mysql/bin/mysql --host=127.0.0.1 --port=11047 -u root --password=$1 < /var/www/eclass/pappabakaliatis.csec.gr/db_backup/original.sql"
+	sh ./deploy.sh
+	ssh pappabakaliatis@pappabakaliatis.csec.gr "/usr/local/mysql/bin/mysql --host=127.0.0.1 --port=11047 -u root --password=$1 < /var/www/eclass/pappabakaliatis.csec.gr/db_backup/original.sql & rm -rf /var/www/eclass/pappabakaliatis.csec.gr/db_backup"
 else
 	echo "Run like this: $0 {database password}"
 fi
